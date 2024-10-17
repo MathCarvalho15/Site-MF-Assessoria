c40b0fd6e9474b2a99513ff6107ee80204ab197f
Matheus Carvalho <theusma.arquitetura@gmail.com>
2024-10-17 16:25:57 -0300

    finalizando css das pages

--- routers/index.js
+++ routers/index.js
@@ -5,15 +5,9 @@ router.get('/',index.index);
 
 router.get('/about',index.about);
 
-router.get('/appointment',index.appointment);
-
 router.get('/contact',index.contact);
 
-router.get('/feature',index.feature);
-
 router.get('/service',index.service);
-
-router.get('/team',index.team);
 
 router.get('/testimonial',index.testimonial);
 
