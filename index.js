be7da99cecd34da74b99a7614dcd755decf6de83
MathCarvalho15 <carvalho2604@outlook.com>
2024-10-28 17:09:22 -0300

    alterando a port

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', './views'); // A pasta onde estão seus arquivos EJS
 
-app.listen(3080, () => {
+app.listen(3000, () => {
     console.log('Servidor rodando na porta 3080!');
 });
